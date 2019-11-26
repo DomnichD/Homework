f113529f0dd8adafce65440a171616be0b2c3df6
Yevhenii Domnich <ydomnich@blhs.org>
2019-11-26 20:27:44 +0200

    project-structure

---
+++ Domnich.Git.Work/Domnich.Git.Work/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domnich.Git.Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
